b803b9d494d0ae7ec18983b16bba6756c006be3d
kennmundell <64756644+kennmundell@users.noreply.github.com>
2021-04-29 15:25:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+def raise_pen():
+    tim.penup()
+
+
+def lower_pen():
+    tim.pendown()
+
+
+screen.listen()
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "s")
+screen.onkey(clear, "c")
+screen.onkey(raise_pen, "u")
+screen.onkey(lower_pen, "l")
+screen.exitonclick()
+
+
